ade9177d79f6f29726c035bfb512e210f56a0f5e
lginzburg2016 <lea.ginzburg@eleves.ec-nantes.fr>
2019-02-20 15:04:58 +0100

    creation d une scene complete avec plusieurs spheres - chapitre 12 du doc - fin du tuto

--- RayTracing/main.cpp
+++ RayTracing/main.cpp
@@ -41,6 +41,40 @@ vec3 color(const ray& r, hitable *world, int depth){
 	}
 }
 
+
+//creation d une scene aleatoire avec de nombreuses spheres
+hitable *random_scene() {
+	int n = 500;
+	hitable **list = new hitable*[n + 1];
+	list[0] = new sphere(vec3(0, 1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+	int i = 1;
+	for (int a = -11; a < 11; a++) {
+		for (int b = -11; b < 11; b++) {
+			float choose_mat = double(rand()) / RAND_MAX;
+			vec3 center(a + 0.9*(double(rand()) / RAND_MAX), 0.2, b + 0.9*(double(rand()) / RAND_MAX));
+			if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+				if (choose_mat < 0.8) { //diffuse
+					list[i++] = new sphere(center, 0.2, new lambertian(vec3( (double(rand())/RAND_MAX)*(double(rand()) / RAND_MAX), (double(rand()) / RAND_MAX)*(double(rand()) / RAND_MAX), (double(rand()) / RAND_MAX)*(double(rand()) / RAND_MAX))));
+				}
+				else if (choose_mat < 0.95) { //metal
+					list[i++] = new sphere(center, 0.2, new metal(vec3(0.5*(1 + (double(rand()) / RAND_MAX)), 0.5*(1 + (double(rand()) / RAND_MAX)), 0.5*(1 + (double(rand()) / RAND_MAX))), 0.5*(double(rand()) / RAND_MAX)));
+				}
+				else{ //verre
+					list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+				}
+			}
+
+		}
+	}
+
+	list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+	list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4,0.2,0.1)));
+	list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7,0.6,0.5), 0.0));
+
+	return new hitable_list(list, i);
+}
+
+
 int main() {
 	ofstream outputfile;
 	//fichier de sortie pour l image coloree souhaitee
@@ -60,6 +94,8 @@ int main() {
 	list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
 	hitable *world = new hitable_list(list,5);
 	
+	world = random_scene();
+
 	//prise en compte ouverture camera
 	vec3 lookfrom(3, 3, 2);
 	vec3 lookat(0, 0, -1);
